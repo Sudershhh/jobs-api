5073f22f51a60274cf3f9b6a767885e6965583e1
Sri Sudersan <tgsuder@gmail.com>
2022-04-16 00:15:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 //connectDB
 const connectDB = require('./db/connect')
@@ -20,9 +24,22 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // extra packages
+
+app.set('trustproxy',1)
+app.use(rateLimiter({
+  windowMs: 15*60*1000,
+  max:100,
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
+
+app.get('/',(req,res)=> {
+  res.send('JOBS API')
+})
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',authenticateUser,jobRouter);
 
